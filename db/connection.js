7e82b947a50afa4224c77e26fba825f829d01daf
MistbornL <lashaloria3@gmail.com>
2022-11-22 16:41:29 +0400

    connection

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { log } = require("mercedlogger");
+require("dotenv").config();
+
+const option = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const { MONGODB_URI } = process.env;
+mongoose.connect(MONGODB_URI, option).then(
+  function () {
+    log.green("Connected to Mongo");
+  },
+  function (err) {
+    log.red("Error connecting to Mongo" + err);
+  }
+);
+
+// EXPORT CONNECTION
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require("dotenv").config(); // load .env variables
 const express = require("express"); // import express
 const { log } = require("mercedlogger"); // import mercedlogger's log function
 const cors = require("cors"); // import cors
-const UserRoutes = require("./controllers/userController"); //import User Routes
-const Users = require("./routes/userRoutes");
-const CollectionRoutes = require("./routes/collectionRoutes");
-const TagRoutes = require("./routes/tagRoutes");
+const http = require("http");
+const { Server } = require("socket.io");
+// const UserRoutes = require("./controllers/userController"); //import User Routes
+// const Users = require("./routes/userRoutes");
+// const CollectionRoutes = require("./routes/collectionRoutes");
+// const TagRoutes = require("./routes/tagRoutes");
 
 //DESTRUCTURE ENV VARIABLES WITH DEFAULT VALUES
 const { PORT = 5000 } = process.env;
@@ -21,10 +23,31 @@ app.get("/", (req, res) => {
   res.send("this is the test route to make sure server is working");
 });
 
-app.use("/users", Users);
-app.use("/collection", CollectionRoutes);
-app.use("/user", UserRoutes);
-app.use("/tag", TagRoutes);
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join_room", (data) => {
+    console.log(data);
+    socket.join(data.room);
+    socket.to(data.room).emit("receive_joining", data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+// app.use("/users", Users);
+// app.use("/collection", CollectionRoutes);
+// app.use("/user", UserRoutes);
+// app.use("/tag", TagRoutes);
 
 // APP LISTENER
 app.listen(PORT, () => log.green("SERVER STATUS", `Listening on port ${PORT}`));
